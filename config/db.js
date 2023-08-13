e26caf7df5fb9503fd5b6de3611bca62d15f4029
Mazhit Alfarabi <a-mazhit@mail.ru>
2023-08-13 23:13:20 +0500

    production mode configured

--- config/db.js
+++ config/db.js
@@ -1,15 +1,29 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config");
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  {
-    host: config.development.host,
-    port: config.development.port,
-    dialect: config.development.dialect,
-  }
-);
+let sequelize;
+if (process.env === "production") {
+  sequelize = new Sequelize(
+    config.production.database,
+    config.production.username,
+    config.production.password,
+    {
+      host: config.production.host,
+      port: config.production.port,
+      dialect: config.production.dialect,
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,
+    {
+      host: config.development.host,
+      port: config.development.port,
+      dialect: config.development.dialect,
+    }
+  );
+}
 sequelize
   .authenticate()
   .then(() => {
